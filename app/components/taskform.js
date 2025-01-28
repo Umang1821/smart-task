b2616ac1cd247bb511090d59f70e470c756dad6f
Umang1821 <umangmishra171@gmail.com>
2025-01-28 19:36:41 +0530

    changes

--- app/components/taskform.js
+++ app/components/taskform.js
@@ -36,7 +36,7 @@ export default function TaskForm({
           />
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
           <select
             value={newItem.priority}
             onChange={(e) =>
